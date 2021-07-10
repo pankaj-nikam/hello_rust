17eb371aa8fc30db89c437b14b5349d43b49ce28
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-07-10 20:02:30 +0530

    Added some more arithmetic operations.

--- arithemetic.rs
+++ arithemetic.rs
@@ -6,4 +6,8 @@ fn main() {
     Then we multiply and divide. Since they are present in this expression, it is evaluated left to right.
     We get 17 % 5 + 600 / 7, which is 17 % 5 + 85, which in turn means 2 + 85 which makes it 87.*/
     println!("{}", (23 - 6) % 5 + 20 * 30 / (3 + 4));
+    //Let us check floating variables:
+    println!("The sum of {} and {} is {}", 90.5, 31.9, 90.5 + 31.9);
+    //This does not round
+    println!("The sum of {} and {} is {}", 80.3, 34.9, 80.3 + 34.9);
 }
