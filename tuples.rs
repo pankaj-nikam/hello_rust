93d922b39aa8b008928296f1a02796786a6152fc
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-08-30 22:44:58 +0530

    Completed basics of Tuple.

--- tuples.rs
+++ tuples.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let data = (3.14, "Mathematics", '🥟');
+    let mut data = (3.14, "Mathematics", '🥟');
+    //You cannot do the following because tuples have fixed types inside it also.
+    // data.0 = "Pankaj";
+    data.1 = "Geometry";
     let copy_of_data = data;
+    let empty_tuple = ();
     println!("{:?}", copy_of_data);
+    println!("{:?}", empty_tuple);
 }
