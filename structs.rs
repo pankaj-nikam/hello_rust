4dd80540bc363e442bf2e3280537b333f438c9d8
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-08-31 11:55:31 +0530

    Basic scenario for structs.

--- structs.rs
+++ structs.rs
@@ -1,13 +1,14 @@
 fn main() {
     let mut pankaj = Person {
-        first_name: "Pankaj".to_string(),
-        last_name: "Nikam".to_string(),
+        first_name: String::from("Pankaj"),
+        last_name: String::from("Nikam"),
         age: 32,
     };
     pankaj.first_name = "Amit".to_string();
-    println!("{:#?}", pankaj.first_name);
+    println!("{:#?}", pankaj);
 }
 
+#[derive(Debug)]
 struct Person {
     first_name: String,
     last_name: String,
