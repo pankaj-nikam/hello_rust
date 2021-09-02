20a272672fa8e79d4cfaef5e955cfe28effd54b7
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-09-02 12:39:44 +0530

    Added functions with parameters.

--- functions.rs
+++ functions.rs
@@ -2,6 +2,15 @@ fn line() {
     println!("Line");
 }
 
+fn add_two_numbers(num1: f64, num2: f64) {
+    println!(
+        "First number is {} and second number is {}, their sum is {}",
+        num1,
+        num2,
+        num1 + num2
+    );
+}
+
 fn main() {
     fn something_else() {
         println!("Something else");
@@ -10,4 +19,5 @@ fn main() {
     line();
     line();
     something_else();
+    add_two_numbers(3.14, 2.22);
 }
